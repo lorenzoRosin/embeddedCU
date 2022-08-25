d3e0311f396a584ffa0d6119f9755b324e5bf5ea
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-08-25 16:34:05 +0200

    CrcTest complete, adding circular queue

--- C/Code/Inc/eCUCrc.h
+++ C/Code/Inc/eCUCrc.h
@@ -32,7 +32,7 @@ extern "C" {
  * GLOBAL PROTOTYPES
  **********************************************************************************************************************/
 /**
- * Calculate the CRC 32 of a passed buffer using as seed the default value of 0xffffffffu
+ * Calculate the CRC 32 (CRC-32/MPEG-2) of a passed buffer using as seed the default value of 0xffffffffu
  * @param data Pointer to the data buffer where we will calculate the CRC 32
  * @param dataLen how many byte will be used to calculate the CRC 32
  * @param crc32Val Pointer to an uint32_t were we will store the calculated CRC 32
@@ -42,7 +42,7 @@ extern "C" {
 e_eCU_Res crc32(const uint8_t* data, const uint32_t dataLen, uint32_t* const crc32Val);
 
 /**
- * Calculate the CRC 32 of a passed buffer using a custom seed
+ * Calculate the CRC 32 (CRC-32/MPEG-2) of a passed buffer using a custom seed
  * @param seed Seed that will be used to calculate the CRC 32
  * @param data Pointer to the data buffer where we will calculate the CRC 32
  * @param dataLen how many byte will be used to calculate the CRC 32
--- C/Test/TestProject/main.c
+++ C/Test/TestProject/main.c
@@ -1,10 +1,13 @@
 
 #include "eCUCrcTest.h"
+#include "eCUCircularQueueTest.h"
+
 int main(void);
 
 
 int main(void)
 {
     crc32Test();
+    circularQueueTest();
     return 0;
 }
