f47b6dca2cd983f085dabdb94ba2d22ff759b67f
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-09-05 22:25:41 +0200

    Digest revisited

--- C/Code/Inc/eCUCrcDigest.h
+++ C/Code/Inc/eCUCrcDigest.h
@@ -24,9 +24,19 @@ extern "C" {
 /***********************************************************************************************************************
  *      TYPEDEFS
  **********************************************************************************************************************/
-typedef e_eCU_Res (*cb_crc32_seed) ( const uint32_t seed, const uint8_t dataS[], const uint32_t dataSLen,
-                                     uint32_t* const crc32SVal );
+typedef e_eCU_Crc_Res (*cb_crc32_seed) ( const uint32_t seed, const uint8_t dataS[], const uint32_t dataSLen,
+                                         uint32_t* const crc32SVal );
 
+typedef enum
+{
+    CRCD_RES_OK = 0,
+    CRCD_RES_BADPARAM,
+    CRCD_RES_BADPOINTER,
+	CRCD_RES_CORRUPTCTX,
+    CRCD_RES_TOOMANYDIGEST,
+    CRCD_RES_NODIGESTDONE,
+    CRCD_RES_NOINITLIB,
+}e_eCU_CrcD_Res;
 
 typedef struct
 {
@@ -46,44 +56,45 @@ typedef struct
  * Initialize the CRC32 digester context ( use as base seed 0xffffffffu )
  * @param ctx Crc digester context
  * @param cbCrcP Pointer to a CRC 32 seed callback function
- * @return ECU_RES_BADPOINTER in case of bad pointer
- *         ECU_RES_OK crc digester is initialized correctly
+ * @return CRCD_RES_BADPOINTER in case of bad pointer
+ *         CRCD_RES_OK crc digester is initialized correctly
  */
-e_eCU_Res crcDigestInitCtx(s_eCU_CrcDigestCtx* const ctx, cb_crc32_seed cbCrcP);
+e_eCU_CrcD_Res crcDigestInitCtx(s_eCU_CrcDigestCtx* const ctx, cb_crc32_seed cbCrcP);
 
 /**
  * Initialize the CRC32 digester context using a selected seed
  * @param ctx Crc digester context
  * @param seed Base Seed
  * @param cbCrcP Pointer to a CRC 32 seed callback function
- * @return ECU_RES_BADPOINTER in case of bad pointer
- *         ECU_RES_OK crc digester is initialized correctly
+ * @return CRCD_RES_BADPOINTER in case of bad pointer
+ *         CRCD_RES_OK crc digester is initialized correctly
  */
-e_eCU_Res crcDigestSeedInitCtx(s_eCU_CrcDigestCtx* const ctx, const uint32_t seed, cb_crc32_seed cbCrcP);
+e_eCU_CrcD_Res crcDigestSeedInitCtx(s_eCU_CrcDigestCtx* const ctx, const uint32_t seed, cb_crc32_seed cbCrcP);
 
 /**
  * Digest a chunk of data in to the CRC32 calc
  * @param ctx Crc digester context
  * @param data Pointer to a memory area containg a chunk of data to analyze
  * @param dataLen Lenght of the buffer we will digest
- * @return ECU_RES_BADPOINTER in case of bad pointer
- *		   ECU_RES_NOINITLIB need to init the lib before taking some action
- *         ECU_RES_OUTOFMEM Too many digest operation
- *		   ECU_RES_BADPARAM in case of an invalid parameter or state
- *         ECU_RES_OK crc digester is initialized correctly
- */
-e_eCU_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, const uint32_t dataLen);
+ * @return CRCD_RES_BADPOINTER in case of bad pointer
+ *		   CRCD_RES_NOINITLIB need to init the lib before taking some action
+ *		   CRCD_RES_BADPARAM in case of an invalid parameter
+ *		   CRCD_RES_CORRUPTCTX in case of an corrupted context
+ *         CRCD_RES_TOOMANYDIGEST Too many digest operation
+ *         CRCD_RES_OK operation ended correctly
+e_eCU_CrcD_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, const uint32_t dataLen);
 
 /**
  * Retrive the CRC32 value calculated till now
  * @param ctx Crc digester context
  * @param crcCalc Pointer to a memory that will contain the value of the calculated CRC
- * @return ECU_RES_BADPOINTER in case of bad pointer
- *		   ECU_RES_NOINITLIB need to init the lib before taking some action
- *		   ECU_RES_BADPARAM in case of an invalid parameter or state
- *         ECU_RES_OK crc digester is initialized correctly
+ * @return CRCD_RES_BADPOINTER in case of bad pointer
+ *		   CRCD_RES_NOINITLIB need to init the lib before taking some action
+ *		   CRCD_RES_CORRUPTCTX in case of an corrupted context
+ *		   CRCD_RES_NODIGESTDONE need to do al least one digest before getting val
+ *         CRCD_RES_OK operation ended correctly
  */
-e_eCU_Res crcDigesGetDigestVal(s_eCU_CrcDigestCtx* const ctx, uint32_t* const crcCalc);
+e_eCU_CrcD_Res crcDigesGetDigestVal(s_eCU_CrcDigestCtx* const ctx, uint32_t* const crcCalc);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- C/Code/Src/eCUCrcDigest.c
+++ C/Code/Src/eCUCrcDigest.c
@@ -20,15 +20,15 @@ static bool_t isCrctatusStillCoherent(const s_eCU_CrcDigestCtx* ctx);
 /***********************************************************************************************************************
  *   GLOBAL FUNCTIONS
  **********************************************************************************************************************/
-e_eCU_Res crcDigestInitCtx(s_eCU_CrcDigestCtx* const ctx, cb_crc32_seed cbCrcP)
+e_eCU_CrcD_Res crcDigestInitCtx(s_eCU_CrcDigestCtx* const ctx, cb_crc32_seed cbCrcP)
 {
 	/* Local variable */
-	e_eCU_Res result;
+	e_eCU_CrcD_Res result;
 
 	/* Check pointer validity */
 	if( ( NULL == ctx) || ( NULL ==  cbCrcP) )
 	{
-		result = ECU_RES_BADPOINTER;
+		result = CRCD_RES_BADPOINTER;
 	}
 	else
 	{
@@ -39,21 +39,21 @@ e_eCU_Res crcDigestInitCtx(s_eCU_CrcDigestCtx* const ctx, cb_crc32_seed cbCrcP)
 		ctx->lastDigest = 0u;
 		ctx->cbCrcPointer = cbCrcP;
 
-		result = ECU_RES_OK;
+		result = CRCD_RES_OK;
     }
 
 	return result;
 }
 
-e_eCU_Res crcDigestSeedInitCtx(s_eCU_CrcDigestCtx* const ctx, const uint32_t seed, cb_crc32_seed cbCrcP)
+e_eCU_CrcD_Res crcDigestSeedInitCtx(s_eCU_CrcDigestCtx* const ctx, const uint32_t seed, cb_crc32_seed cbCrcP)
 {
 	/* Local variable */
-	e_eCU_Res result;
+	e_eCU_CrcD_Res result;
 
 	/* Check pointer validity */
 	if( ( NULL == ctx) || ( NULL ==  cbCrcP) )
 	{
-		result = ECU_RES_BADPOINTER;
+		result = CRCD_RES_BADPOINTER;
 	}
 	else
 	{
@@ -64,50 +64,51 @@ e_eCU_Res crcDigestSeedInitCtx(s_eCU_CrcDigestCtx* const ctx, const uint32_t see
 		ctx->lastDigest = 0u;
 		ctx->cbCrcPointer = cbCrcP;
 
-		result = ECU_RES_OK;
+		result = CRCD_RES_OK;
     }
 
 	return result;
 }
 
-e_eCU_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, const uint32_t dataLen)
+e_eCU_CrcD_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, const uint32_t dataLen)
 {
 	/* Local variable */
-	e_eCU_Res result;
+	e_eCU_CrcD_Res result;
+	e_eCU_Crc_Res crcRes;
     uint32_t cR32;
 
 	/* Check pointer validity */
 	if( ( NULL == ctx ) || ( NULL == data ) )
 	{
-		result = ECU_RES_BADPOINTER;
+		result = CRCD_RES_BADPOINTER;
 	}
 	else
 	{
 		/* Check Init */
 		if( false == ctx->isInit )
 		{
-			result = ECU_RES_NOINITLIB;
+			result = CRCD_RES_NOINITLIB;
 		}
 		else
 		{
 			/* Check data validity */
 			if( dataLen <= 0u )
 			{
-				result = ECU_RES_BADPARAM;
+				result = CRCD_RES_BADPARAM;
 			}
 			else
 			{
                 /* Check internal status validity */
                 if( false == isCrctatusStillCoherent(ctx) )
                 {
-                    result = ECU_RES_BADPARAM;
+                    result = CRCD_RES_CORRUPTCTX;
                 }
                 else
                 {
                     /* Check if we have memory for this */
                     if( ctx->digestedTimes >= 0xFFFFFFFFu )
                     {
-                        result = ECU_RES_OUTOFMEM;
+                        result = CRCD_RES_TOOMANYDIGEST;
                     }
                     else
                     {
@@ -115,24 +116,34 @@ e_eCU_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, con
                         if( 0u == ctx->digestedTimes )
                         {
                             /* Use base seed */
-                            result = (*(ctx->cbCrcPointer))( ctx->usedBaseSeed, data, dataLen, &cR32 );
+                            crcRes = (*(ctx->cbCrcPointer))( ctx->usedBaseSeed, data, dataLen, &cR32 );
 
-                            if( ECU_RES_OK == result )
+                            if( CRC_RES_OK == result )
                             {
                                 ctx->digestedTimes++;
                                 ctx->lastDigest = cR32;
+								result = CRCD_RES_OK;
                             }
+							else
+							{
+								result = CRCD_RES_BADPOINTER;
+							}
                         }
                         else
                         {
                             /* Continue calc */
-                            result = (*(ctx->cbCrcPointer))( ctx->lastDigest, data, dataLen, &cR32 );
+                            crcRes = (*(ctx->cbCrcPointer))( ctx->lastDigest, data, dataLen, &cR32 );
 
-                            if( ECU_RES_OK == result )
+                            if( CRC_RES_OK == result )
                             {
                                 ctx->digestedTimes++;
                                 ctx->lastDigest = cR32;
+								result = CRCD_RES_OK;
                             }
+							else
+							{
+								result = CRCD_RES_BADPOINTER;
+							}
                         }
                     }
                 }
@@ -143,36 +154,36 @@ e_eCU_Res crcDigesDigest(s_eCU_CrcDigestCtx* const ctx, const uint8_t* data, con
 	return result;
 }
 
-e_eCU_Res crcDigesGetDigestVal(s_eCU_CrcDigestCtx* const ctx, uint32_t* const crcCalc)
+e_eCU_CrcD_Res crcDigesGetDigestVal(s_eCU_CrcDigestCtx* const ctx, uint32_t* const crcCalc)
 {
 	/* Local variable */
-	e_eCU_Res result;
+	e_eCU_CrcD_Res result;
 
 	/* Check pointer validity */
 	if( ( NULL == ctx ) || ( NULL == crcCalc ) )
 	{
-		result = ECU_RES_BADPOINTER;
+		result = CRCD_RES_BADPOINTER;
 	}
 	else
 	{
 		/* Check Init */
 		if( false == ctx->isInit )
 		{
-			result = ECU_RES_NOINITLIB;
+			result = CRCD_RES_NOINITLIB;
 		}
 		else
 		{
             /* Check internal status validity */
             if( false == isCrctatusStillCoherent(ctx) )
             {
-                result = ECU_RES_BADPARAM;
+                result = CRCD_RES_CORRUPTCTX;
             }
 			else
 			{
                 /* Check data validity */
                 if( ctx->digestedTimes <= 0u )
                 {
-                    result = ECU_RES_BADPARAM;
+                    result = CRCD_RES_NODIGESTDONE;
                 }
                 else
                 {
@@ -183,7 +194,7 @@ e_eCU_Res crcDigesGetDigestVal(s_eCU_CrcDigestCtx* const ctx, uint32_t* const cr
                     ctx->digestedTimes = 0u;
                     ctx->lastDigest = 0u;
 
-                    result = ECU_RES_OK;
+                    result = CRCD_RES_OK;
                 }
 			}
 		}
