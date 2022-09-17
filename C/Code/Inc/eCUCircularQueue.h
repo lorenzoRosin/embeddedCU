a5d8e351b7e547e5b4755644a0126741b35ea7a4
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-09-18 00:25:59 +0200

    Circulat queue header updated

--- C/Code/Inc/eCUCircularQueue.h
+++ C/Code/Inc/eCUCircularQueue.h
@@ -1,7 +1,11 @@
 /**
- * @file eCUCircularQueue.h
+ * @file       eCUCircularQueue.h
  *
- */
+ * @brief      Circular Queue utils
+ *
+ * @author     Lorenzo Rosin
+ *
+ **********************************************************************************************************************/
 
 #ifndef ECUCIRCULARQUEUE_H
 #define ECUCIRCULARQUEUE_H
@@ -51,87 +55,101 @@ typedef struct
  * GLOBAL PROTOTYPES
  **********************************************************************************************************************/
 /**
- * Initialize the circular queue context
- * @param ctx Circular queue context
- * @param memPool Pointer to a memory pool that we will use to manage the circular queue
- * @param memPoolSize Dimension in byte of the circular queue
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_BADPARAM in case of an invalid parameter
- *         CQUEUE_RES_OK circular queue is initialized correctly
+ * @brief       Initialize the circular queue context
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[in]   memPool     - Pointer to a memory buffer that we will use to manage the circular queue
+ * @param[out]  memPoolSize - Dimension in byte of the circular queue
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *              CQUEUE_RES_BADPARAM     - In case of bad parameter passed to the function
+ *              CQUEUE_RES_OK           - Circular queue initialized successfully
  */
 e_eCU_cQueue_Res circQInitCtx(s_eCU_circQCtx* const ctx, uint8_t* memPool, const uint32_t memPoolSize);
 
 /**
- * Reset the state of the circular queue and discharge all saved data
- * @param ctx Circular queue context
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *         CQUEUE_RES_OK circular queue is reset correctly
+ * @brief       Reset the state of the circular queue and discharge all saved data
+ *
+ * @param[in]   ctx         - Circular queue context
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Circular queue resetted successfully
  */
 e_eCU_cQueue_Res circQReset(s_eCU_circQCtx* const ctx);
 
 /**
- * Get free space present in the queue
- * @param ctx Circular queue context
- * @param freeSpace Pointer to variable where free space present in queue will be stored
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- * 		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *         CQUEUE_RES_OK operation returned correctly
+ * @brief       Get free space present in the queue
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[out]  freeSpace  - Pointer to variable where free space present in queue will be stored
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Operation ended successfully
  */
 e_eCU_cQueue_Res circQGetFreeSapce(s_eCU_circQCtx* const ctx, uint32_t* const freeSpace);
 
 /**
- * Get the used space in the queue
- * @param ctx Circular queue context
- * @param usedSpace Pointer to variable where used space present in queue will be stored
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *         CQUEUE_RES_OK operation returned correctly
+ * @brief       Get used space present in the queue
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[out]  usedSpace  - Pointer to variable where used space present in queue will be stored
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Operation ended successfully
  */
 e_eCU_cQueue_Res circQGetOccupiedSapce(s_eCU_circQCtx* const ctx, uint32_t* const usedSpace);
 
 /**
- * Insert data in the queue if free space is avaiable
- * @param ctx Circular queue context
- * @param data Pointer to the data that we want to insert in the queue
- * @param datalen Lenght of the data present in the pointer passed
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *         CQUEUE_RES_BADPARAM in case of an invalid parameter
- *		   CQUEUE_RES_FULL queue full, can't push data so much
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *         CQUEUE_RES_OK operation returned correctly
+ * @brief       Insert data in the queue if free space is avaiable
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[in]   data        - Pointer to the data that we want to insert in the queue
+ * @param[in]   datalen     - Lenght of the data present in the pointer passed
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *              CQUEUE_RES_BADPARAM     - In case of bad parameter passed to the function
+ *		        CQUEUE_RES_FULL         - Queue full, can't push this amount of data
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Operation ended successfully
  */
 e_eCU_cQueue_Res circQInsertData(s_eCU_circQCtx* const ctx, const uint8_t* data, const uint32_t datalen);
 
 /**
- * Retrive data from the queue if avaiable
- * @param ctx Circular queue context
- * @param data Pointer to the data that we want to retrive from queue
- * @param datalen Lenght of the data that we want to retrive
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *		   CQUEUE_RES_BADPARAM in case of an invalid parameter 
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *		   CQUEUE_RES_EMPTY not so many byte present in queue
- *         CQUEUE_RES_OK operation returned correctly
+ * @brief       Retrive data from the queue if avaiable
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[out]  data        - Pointer to the buffer that will contain retrived data from queue
+ * @param[in]   datalen     - Lenght of the data that we want to retrive
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *              CQUEUE_RES_BADPARAM     - In case of bad parameter passed to the function
+ *		        CQUEUE_RES_EMPTY        - Not so many byte present in queue
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Operation ended successfully
  */
 e_eCU_cQueue_Res circQRetriveData(s_eCU_circQCtx* const ctx, uint8_t* const data, const uint32_t datalen);
 
 /**
- * Peek data from the queue if avaiable
- * @param ctx Circular queue context
- * @param data Pointer to the data that we want to peek from queue
- * @param datalen Lenght of the data that we want to peek
- * @return CQUEUE_RES_BADPOINTER in case of bad pointer
- *		   CQUEUE_RES_NOINITLIB need to init the queue before taking some action
- *		   CQUEUE_RES_BADPARAM in case of an invalid parameter 
- *		   CQUEUE_RES_CORRUPTCTX in case of an corrupted context
- *		   CQUEUE_RES_EMPTY not so many byte present in queue
- *         CQUEUE_RES_OK operation returned correctly
+ * @brief       Peek data from the queue if avaiable
+ *
+ * @param[in]   ctx         - Circular queue context
+ * @param[out]  data        - Pointer to the buffer that will contain retrived data from queue
+ * @param[in]   datalen     - Lenght of the data that we want to peek
+ *
+ * @return      CQUEUE_RES_BADPOINTER   - In case of bad pointer passed to the function
+ *		        CQUEUE_RES_NOINITLIB    - Need to init the queue before taking some action
+ *              CQUEUE_RES_BADPARAM     - In case of bad parameter passed to the function
+ *		        CQUEUE_RES_EMPTY        - Not so many byte present in queue
+ *		        CQUEUE_RES_CORRUPTCTX   - In case of an corrupted context
+ *              CQUEUE_RES_OK           - Operation ended successfully
  */
 e_eCU_cQueue_Res circQPeekData(s_eCU_circQCtx* const ctx, uint8_t* const data, const uint32_t datalen);
 
