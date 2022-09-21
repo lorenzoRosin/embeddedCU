5426f2efba380678c2f0f946b1439c440e93204c
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-09-21 23:09:05 +0200

    Start adding byte unstuffer private header

---
+++ C/Code/Inc/eCUByteUnStuffer_prv.h
@@ -0,0 +1,41 @@
+/**
+ * @file       eCUByteUnStuffer_prv.h
+ *
+ * @brief      Byte Unstuffer utils private definition
+ *
+ * @author     Lorenzo Rosin
+ *
+ **********************************************************************************************************************/
+
+#ifndef ECUBYTEUNSTUFFER_PRV_H
+#define ECUBYTEUNSTUFFER_PRV_H
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/***********************************************************************************************************************
+ *      PRIVATE TYPEDEFS
+ **********************************************************************************************************************/
+typedef enum
+{
+    DBUSTF_SM_PRV_NEEDSOF = 0,
+    DBUSTF_SM_PRV_NEEDRAWDATA,
+    DBUSTF_SM_PRV_NEEDNEGATEPRECDATA,
+	DBUSTF_SM_PRV_NEEDEOF,
+    DBUSTF_SM_PRV_STUFFEND,
+}e_eCU_dBUStf_sm_prv;
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+
+
+#endif /* ECUBYTEUNSTUFFER_PRV_H */
