faa9d16420e7d38e911dbc7717397fd90fac0973
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-10-30 11:06:21 +0100

    Style review

--- C/Code/Inc/eCUCrc.h
+++ C/Code/Inc/eCUCrc.h
@@ -28,7 +28,7 @@ extern "C" {
 /***********************************************************************************************************************
  *      DEFINES
  **********************************************************************************************************************/
-#define ECU_CRC_BASE_SEED                               ( 0xffffffffu )
+#define ECU_CRC_BASE_SEED                                                                    ( (uint32_t ) 0xFFFFFFFFu )
 
 
 
@@ -47,9 +47,9 @@ typedef enum
  * GLOBAL PROTOTYPES
  **********************************************************************************************************************/
 /**
- * @brief       Calculate the CRC 32 (CRC-32/MPEG-2) of a passed buffer using as seed the default value of 0xffffffffu
+ * @brief       Calculate the CRC 32 (CRC-32/MPEG-2) of a passed buffer using as seed the default value of 0xFFFFFFFFu
  *
- * @param[in]   data        - Pointer to the data buffer where we will calculate the CRC 32
+ * @param[in]   data        - Pointer to the data buffer used to calculate CRC 32
  * @param[in]   dataLen     - How many byte will be used to calculate the CRC 32
  * @param[out]  crc32Val    - Pointer to an uint32_t were we will store the calculated CRC 32
  *
@@ -63,7 +63,7 @@ e_eCU_Crc_Res crc32(const uint8_t data[], const uint32_t dataLen, uint32_t* cons
  * @brief       Calculate the CRC 32 (CRC-32/MPEG-2) of a passed buffer using a custom seed
  *
  * @param[in]   seed        - Seed that will be used to calculate the CRC 32
- * @param[in]   data        - Pointer to the data buffer where we will calculate the CRC 32
+ * @param[in]   data        - Pointer to the data buffer used to calculate CRC 32
  * @param[in]   dataLen     - How many byte will be used to calculate the CRC 32
  * @param[out]  crc32Val    - Pointer to an uint32_t were we will store the calculated CRC 32
  *
@@ -71,6 +71,8 @@ e_eCU_Crc_Res crc32(const uint8_t data[], const uint32_t dataLen, uint32_t* cons
  *              CRC_RES_OK              - Crc 32 calculated successfully
  */
 e_eCU_Crc_Res crc32Seed(const uint32_t seed, const uint8_t dataS[], const uint32_t dataSLen, uint32_t* const crc32SVal);
+
+
 
 #ifdef __cplusplus
 } /* extern "C" */
