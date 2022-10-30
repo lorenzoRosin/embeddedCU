fffdada3dc55450f5ca4b1f3772b1aca20dba875
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-10-30 17:36:19 +0100

    style

--- C/Code/Src/eCUCircularQueue.c
+++ C/Code/Src/eCUCircularQueue.c
@@ -389,7 +389,7 @@ e_eCU_cQueue_Res circQPeekData(s_eCU_circQCtx* const ctx, uint8_t* const data, c
                     result = circQGetOccupiedSapce(ctx, &usedSpace);
                     if( CQUEUE_RES_OK == result )
                     {
-                        if( datalen > usedSpace)
+                        if( datalen > usedSpace )
                         {
                             /* No enoght data in the queue */
                             result = CQUEUE_RES_EMPTY;
